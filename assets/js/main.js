dc4387b399159dc55f518cfb4a52cf25bcc23730
itsandromeda <itsandromeda@gmail.com>
2017-04-19 13:50:06 -0500

    Copyright text

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,9 @@ var container = document.createElement("div"),
     sideFieldB = document.createElement("div"),
     ballA = document.createElement("div"),
     ballB = document.createElement("div"),
-    ballC = document.createElement("div");
+    ballC = document.createElement("div"),
+    text = document.createTextNode("© Laboratoria 2017"),
+    textContainer = document.createElement("span");
 
 container.classList.add("main-wrapper");
 container.classList.add("field");
@@ -26,12 +28,15 @@ ballB.classList.add("ball");
 ballB.classList.add("ball-mid");
 ballC.classList.add("ball");
 ballC.classList.add("ball-left");
+textContainer.classList.add("copyright");
 
+textContainer.appendChild(text);
 sideFieldA.appendChild(ballA);
 center.appendChild(ballB);
 sideFieldB.appendChild(ballC);
 subContainer.appendChild(center);
 subContainer.appendChild(sideFieldA);
 subContainer.appendChild(sideFieldB);
+subContainer.appendChild(textContainer);
 container.appendChild(subContainer);
 document.body.appendChild(container);
